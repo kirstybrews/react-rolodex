c0252410585eb82ee2b0adb5e353800adabd1c16
Joseph-Burks <joe_burks_3@yahoo.com>
2020-11-03 15:21:38 -0600

    added sort, filter, and search

--- rolodex-react/src/App.js
+++ rolodex-react/src/App.js
@@ -1,71 +1,134 @@
-import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
-import CardBox from "./components/CardBox";
-import Form from "./components/Form";
-import React from "react";
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import CardBox from './components/CardBox';
+import Form from './components/Form';
+import React from 'react';
 
-const peopleUrl = "http://localhost:3000/people"
+const peopleUrl = 'http://localhost:3000/people';
 class App extends React.Component {
-  state = {
-    allPeople: [],
-    filter: "",
-    search: "",
-    sort: ""
+	state = {
+		allPeople: [],
+		filter: '',
+		search: '',
+		sort: '',
+	};
+
+	componentDidMount = () => {
+		fetch(peopleUrl)
+			.then(resp => resp.json())
+			.then(allPeople => this.setState({ allPeople }));
+	};
+
+	addPerson = newPerson => {
+		// console.log(newPerson)
+		fetch(peopleUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify(newPerson),
+		})
+			.then(res => res.json())
+			.then(postedPerson => {
+				this.setState({
+					allPeople: [...this.state.allPeople, postedPerson],
+				});
+			});
+	};
+
+	editPerson = editedPerson => {
+		console.log('Edit in APP');
+		console.log(editedPerson);
+		fetch(`${peopleUrl}/${editedPerson.id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify(editedPerson),
+		})
+			.then(res => res.json())
+			.then(serverPerson => {
+				this.setState({
+					allPeople: this.state.allPeople.map(peep =>
+						peep.id == serverPerson.id ? editedPerson : peep
+					),
+				});
+			});
+	};
+
+	handleSearchChange = term => {
+		let search = term.toLowerCase();
+		this.setState({ search });
+  };
+  
+  handleSortChange = value => {
+    this.setState({ sort: value })
   }
 
-  componentDidMount = () => {
-    fetch(peopleUrl)
-    .then(resp=>resp.json())
-    .then(allPeople => this.setState({allPeople}))
-  }
+  sortPeople = () => {
+		let peeps = this.state.allPeople;
+		switch (this.state.sort) {
+			case 'Alphabetically':
+				return peeps.sort((a, b) => {
+					return a.name.localeCompare(b.name);
+				});
+			case 'None':
+				return peeps;
+			default:
+				return peeps;
+		}
 
-  addPerson = (newPerson) => {
-    // console.log(newPerson)
-    fetch(peopleUrl, {
-      method: 'POST',
-      headers: {
-        "Content-Type":'application/json',
-        "Accept":'application/json'
-      },
-      body:JSON.stringify(newPerson)
-    })
-    .then(res=> res.json())
-    .then(postedPerson => {
-      this.setState({
-        allPeople: [...this.state.allPeople, postedPerson]
-      })
-    })
-  }
-
-  editPerson = (editedPerson) => {
-    console.log("Edit in APP")
-    console.log(editedPerson)
-    fetch(`${peopleUrl}/${editedPerson.id}`,{
-      method: 'PATCH',
-      headers: {
-        "Content-Type":'application/json',
-        "Accept":'application/json'
-      },
-      body: JSON.stringify(editedPerson)
-    })
-    .then(res=> res.json())
-    .then(serverPerson => {
-      this.setState({
-      allPeople: this.state.allPeople.map(peep => peep.id == serverPerson.id ? editedPerson : peep)
-    })
-    })
+    // ^^ same as vv
     
+      // if(this.state.sort === 'Alphabetically'){
+      //    return peeps.sort((a, b) => {
+      // 		  return a.name.localeCompare(b.name);
+      //    }
+      // }else if(this.state.sort === 'None'){
+      //   return peeps;
+      // }else{
+      //   return peeps;
+      // }
+	}
+
+  handleFilterChange = value => {
+    this.setState({filter: value})
   }
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <SearchBar />
-        <CardBox people={this.state.allPeople} editPerson = {this.editPerson}/>
-        <Form addPerson = {this.addPerson}/>
-      </div>
-    );
+
+  filterPeople = () => {
+    let peeps = this.sortPeople()
+    switch(this.state.filter){
+      case 'All':
+        return peeps
+      case 'Alumni':
+        return peeps.filter(peep => peep['alum?'])
+      case 'Instructors':
+        return peeps.filter(peep => peep['instructor?']);
+      default:
+        return peeps
+    }
   }
+
+	render() {
+		let people = this.filterPeople().filter(peep =>
+			peep.name.toLowerCase().includes(this.state.search)
+		);
+		return (
+			<div className='App'>
+				<Header />
+				<SearchBar
+					handleSearchChange={this.handleSearchChange}
+					handleSortChange={this.handleSortChange}
+          sort={this.state.sort}
+          handleFilterChange={this.handleFilterChange}
+				/>
+				<CardBox people={people} editPerson={this.editPerson} />
+				<Form addPerson={this.addPerson} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- rolodex-react/src/components/SearchBar.js
+++ rolodex-react/src/components/SearchBar.js
@@ -3,40 +3,46 @@ import React from 'react'
 class SearchBar extends React.Component{
 
     render(){
-        return(
-            <div>
-      <label>
-        <strong>Search People</strong>
-        <input onChange={null} />
-      </label>
-      <br />
-      <strong>Sort by:</strong>
-      <label>
-        <input
-          type="radio"
-          value="Alphabetically"
-          checked={null}
-          onChange={null}
-        />
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="None" checked={null} onChange={null} />
-        None
-      </label>
-      <br />
+        return (
+					<div>
+						<label>
+							<strong>Search People</strong>
+							<input
+								onChange={e => this.props.handleSearchChange(e.target.value)}
+							/>
+						</label>
+						<br />
+						<strong>Sort by:</strong>
+						<label>
+							<input
+								type='radio'
+								value='Alphabetically'
+								checked={this.props.sort === 'Alphabetically'}
+								onChange={e => this.props.handleSortChange(e.target.value)}
+							/>
+							Alphabetically
+						</label>
+						<label>
+							<input
+								type='radio'
+								value='None'
+								checked={this.props.sort === 'None'}
+								onChange={e => this.props.handleSortChange(e.target.value)}
+							/>
+							None
+						</label>
+						<br />
 
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="All">All</option>
-          <option value="Students">Students</option>
-          <option value="Alumni">Alumni</option>
-          <option value="Instructors">Instructors</option>
-        </select>
-      </label>
-    </div>
-        )
+						<label>
+							<strong>Filter:</strong>
+							<select onChange={e => this.props.handleFilterChange(e.target.value)}>
+								<option value='All'>All</option>
+								<option value='Alumni'>Alumni</option>
+								<option value='Instructors'>Instructors</option>
+							</select>
+						</label>
+					</div>
+				);
     }
 }
 
