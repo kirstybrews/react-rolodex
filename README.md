59378e4eab44ae2a495b5ee26d9db8ac6ac49626
Raul Sanchez <raulsg93@gmail.com>
2020-11-03 09:42:59 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Use this from notes Dane!
---
+++ rolodex-react/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div class="jumbotron">
+      <h1 class="display-4">
+        Raul's Rolodex: A Celebration of Under-Represented People in Tech
+      </h1>
+      <hr class="my-4" />
+      <h3>Load of past alum from our db.json file to the container below</h3>
+    </div>
+  );
+};
+
+export default Header
